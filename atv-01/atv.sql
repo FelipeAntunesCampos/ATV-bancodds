646bb18774a12b8a2536a053f6db98840e03195b
Felipe Antunes  Campos <felipe.a.campos7@aluno.senai.br>
2025-09-29 10:34:05 -0300

    feat: adiciona drawio

--- atv-01/atv.sql
+++ atv-01/atv.sql
@@ -70,3 +70,5 @@ DELETE FROM produtos WHERE estoque <= 10;
 DELETE FROM produtos WHERE preco < 20;
 
 DELETE FROM produtos WHERE marca = 'Boca Rosa';
+
+DELETE FROM produtos WHERE estoque > 1 and estoque < 5;
